aa83d886952c03140d6ab7270412ae3ffe1703b7
Andrey Vershinin <adw006@ya.ru>
2020-02-17 21:03:21 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,116 @@
           <h1 class="user-info__name">Jaques Causteau</h1>
           <p class="user-info__job">Sailor, Researcher</p>
         </div>
-        <button class="button user-info__button">+</button>
+        <button @click="isPopupShown=true" class="button user-info__button">+</button>
       </div>
     </div>
 
-    <div class="places-list root__section"></div>
+    <div class="places-list root__section">
+      <card
+        v-for="card in cards"
+        :key="card.id"
+        :name="card.name"
+        :url="card.link"
+        @remove="deleteCard(card.id)"
+      />
+    </div>
+
+    <popup v-if="isPopupShown" @hide-popup="isPopupShown=false" @add-card="addCard"/>
   </div>
 </template>
 
 <script>
+import Card from "./components/Card.vue";
+import Popup from "./components/Popup.vue";
+
 export default {
-  name: "App"
+  name: "App",
+
+  components: {
+    Card,
+    Popup,
+  },
+
+  data() {
+    return {
+      cards: [
+        {
+          id: 1,
+          name: "Архыз",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+        },
+        {
+          id: 2,
+          name: "Челябинская область",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg"
+        },
+        {
+          id: 3,
+          name: "Иваново",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg"
+        },
+        {
+          id: 4,
+          name: "Камчатка",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg"
+        },
+        {
+          id: 5,
+          name: "Холмогорский район",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg"
+        },
+        {
+          id: 6,
+          name: "Байкал",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg"
+        },
+        {
+          id: 7,
+          name: "Нургуш",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg"
+        },
+        {
+          id: 8,
+          name: "Тулиновка",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg"
+        },
+        {
+          id: 9,
+          name: "Остров Желтухина",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg"
+        },
+        {
+          id: 10,
+          name: "Владивосток",
+          link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg"
+        }
+      ],
+
+      isPopupShown: false,
+    };
+  },
+
+  methods: {
+    deleteCard(id) {
+      this.cards = this.cards.filter(card => card.id !== id);
+    },
+
+    addCard(data) {
+      const { name, link } = data;
+      const id = Date.now();
+      this.cards.push({ name, link, id });
+    }
+  }
 };
 </script>
 
---
+++ src/components/Popup.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="popup">
+    <div class="popup__content">
+      <img @click="hide" src="../assets/images/close.svg" alt class="popup__close" />
+      <h3 class="popup__title">Новое место</h3>
+      <form @submit.prevent="addCard" class="popup__form" name="new">
+        <input
+          v-model="name"
+          type="text"
+          name="name"
+          class="popup__input popup__input_type_name"
+          placeholder="Название"
+        />
+        <input
+          v-model="link"
+          type="text"
+          name="link"
+          class="popup__input popup__input_type_link-url"
+          placeholder="Ссылка на картинку"
+        />
+        <button type="submit" class="button popup__button">+</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Popup",
+
+  data() {
+    return {
+      name: "",
+      link: ""
+    };
+  },
+
+  methods: {
+    hide() {
+      this.$emit("hide-popup");
+    },
+
+    addCard() {
+      this.hide();
+      const cardData = {
+        name: this.name,
+        link: this.link
+      };
+
+      this.$emit("add-card", cardData);
+    }
+  }
+};
+</script>
